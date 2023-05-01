dfadbe04e584da3d9bc7f3d6b74fec4c3575fa7d
Brandon <bsm2112@yahoo.com>
2023-04-30 20:40:52 -0500

    fix errors

--- pages/bots/[botId]/index.js
+++ pages/bots/[botId]/index.js
@@ -1,7 +1,4 @@
-import Layout from '../../../components/layout'
 import Head from 'next/head'
-import Date from '../../components/date'
-import utilStyles from '../../styles/utils.module.css'
 
 function BotPage() {
   return (
