31bb8b258514708e3dfe5cbfdda1307fc716120c
Brandon <bsm2112@yahoo.com>
2023-06-19 19:10:21 -0500

    update favicon

--- components/layout.js
+++ components/layout.js
@@ -13,7 +13,7 @@ export default function Layout({ children }) {
   const theme = useContext(ThemeContext);
   return (
     <div className="content">
-      <Head><link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💹</text></svg>"></link></Head>
+      <Head><link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🤑</text></svg>"></link></Head>
       <Navigation />
       <main>{children}</main>
     </div>
