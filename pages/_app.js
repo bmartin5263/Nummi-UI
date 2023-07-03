444b2eadf533f3600bd645e8d94a0c0f08fe1a7e
bmartin5263 <bsm2112@yahoo.com>
2023-07-02 23:51:36 -0500

    fix css

--- pages/_app.js
+++ pages/_app.js
@@ -10,24 +10,46 @@ export default function App({ Component, pageProps: { session, ...pageProps } })
   const log = useLog("App");
   const [darkMode, setDarkMode] = useState(false);
   const [interval, setInterval] = useState(0);
+  const [ready, setReady] = useState(0);
   
   const toggleDarkMode = () => {
     log("Toggling Dark Mode");
     setDarkMode(!darkMode);
   };
 
+  // const theme = window.localStorage.getItem("theme");
+  // if (theme == "dark" && !darkMode) {
+  //   setDarkMode(true);
+  // }
+  // else if (theme == "light" && darkMode) {
+  //   setDarkMode(false);
+  // }
+
+  // useEffect(() => { 
+  //   log("Checking for Body Dark Mode");
+  //   const theme = window.localStorage.getItem("theme");
+  //   if (theme == "dark") {
+  //     setDarkMode(true);
+  //   }
+  //   else if (theme == "light") {
+  //     setDarkMode(false);
+  //   }
+  //   setReady(true);
+  // }, []);
+
   useEffect(() => { 
-    log("Checking for Body Dark Mode");
     if (darkMode) {
       document.querySelector("body").classList.add("dark");
+      window.localStorage.setItem("theme", "dark");
     }
     else {
       document.querySelector("body").classList.remove("dark");
+      window.localStorage.setItem("theme", "light");
     }
   }, [darkMode]);
   return (
     <SessionProvider session={session} refetchInterval={interval}>
-    <ThemeContext.Provider value={{ darkMode, toggleDarkMode }}>
+      <ThemeContext.Provider value={{ darkMode, toggleDarkMode }}>
         <Layout>
           <Component {...pageProps} />
           <RefreshTokenHandler setInterval={setInterval} />
